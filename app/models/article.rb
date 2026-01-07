15381fce69797224ebf250f3582322ab6efd0ed6
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-07 18:59:55 +0900

    ユーザをauthornameに修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -24,6 +24,10 @@ class Article < ApplicationRecord
 
   validate :title_length_and_content_length
 
+  def author_name
+    user.display_name
+  end
+
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
